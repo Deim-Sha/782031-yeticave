2f6f6be0d9598b1f1e6ad6a84dc3a77e64aa3ad5
Deim-Sha <deim.sha@gmail.com>
2019-02-25 16:30:29 +0300

    подключает базу данных, выводит список категорий и лотов из БД

--- functions.php
+++ functions.php
@@ -16,6 +16,39 @@ function include_template($name, $data) {
     return $result;
 }
 
+function setDbConnection($db_config)
+{
+    $link = mysqli_connect($db_config['host'], $db_config['user'], $db_config['password'], $db_config['name']);
+
+    if (!$link) {
+        die("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+    }
+    mysqli_set_charset($link, 'utf8');
+    return $link;
+}
+
+function getCategories($link)
+{
+    $sql = 'SELECT * FROM categories';
+    $result = mysqli_query($link, $sql);
+    $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $categories;
+}
+
+function getLots($link)
+{
+    $sql = 'SELECT l.name, price, img, c.name AS category, end_time
+    FROM lots l
+      JOIN categories c
+      ON l.category_id = c.id
+    WHERE l.winner_id IS NULL
+    GROUP BY l.id
+    ORDER BY l.create_time DESC';
+    $result = mysqli_query($link, $sql);
+    $lots = mysqli_fetch_all($result, MYSQLI_ASSOC);
+    return $lots;
+}
+
 function formatPrice($price)
 {
     $price_int = ceil($price);
@@ -25,11 +58,11 @@ function formatPrice($price)
     return number_format($price_int, 0, null, ' ') . " &#8381";
 }
 
-function createTimer()
+function createTimer($end_time)
 {
     $time_now = date_create("now");
-    $time_end = date_create("tomorrow");
+    $time_end = date_create($end_time);
     $time_diff = date_diff($time_end, $time_now);
 
-    return date_interval_format($time_diff, "%H:%I");
+    return date_interval_format($time_diff, "%Dд %H:%I");
 }
